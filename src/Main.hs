4ee238b3578957c9e3372a6525ba1a0e56d5943c
rethab <rethab@rethab.ch>
2013-03-30 16:07:45 +0100

    basic cli

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import System.Directory          (getCurrentDirectory)
+import System.Environment        (getArgs)
+import Tagger.Crawler            (listArtists)
+import Tagger.IncompletionFinder (getIncomplete)
+import Tagger.TagCompleter       (complete)
+import Tagger.TagWriter          (writeTags)
 
-main = putStrLn "Tagger"
+main :: IO ()
+main = do args <- getArgs
+          dir <- if null args then getCurrentDirectory else return (head args)
+          putStrLn "Tagger 0.1, (c) rethab 2013"
+          putStr ("Search Artists in " ++ dir ++ ".. ")
+          artists <- listArtists dir
+          putStrLn ("found " ++ show (length artists) ++ " Artists.")
+          putStr "Filter incomplete.. "
+          incomplete <- getIncomplete artists 
+          putStrLn (show (length incomplete) ++ " are incomplete.")
+          putStr ("Complete ID3 Tags.. ")
+          completed <- complete incomplete
+          putStrLn "done."
+          putStrLn "Write back? "
+          wb <- getLine
+          if (wb `elem` ["yes", "y"])
+              then do putStr "Write back.. "
+                      writeTags completed
+                      putStrLn "done."
+              else do putStrLn "No writing back: "
+                      mapM_ (putStrLn . show) completed
